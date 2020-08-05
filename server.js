9df155c0e485013a6fb366927e9a55942d0fecad
Robert Luttig <robert.luttig@gmail.com>
2020-08-04 20:12:14 -0700

    added mongodb connection to server

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
